ce29c4c63b49f4c5655a3a4ec709ce40ea529776
jason <jasonwitek2007@gmail.com>
2026-04-08 23:19:24 -0500

    add function stubs

--- fft/src/main.rs
+++ fft/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// this will be our main function
+// We should have it return the DFT vector so we can print it out in our main function
+fn fft() {
+
+}
+
+// this is the twiddle factor helper
+fn twiddle_factor() {
+
+}
+
+// this is the bit reversal
+fn bit_reversal() {
+
+}
