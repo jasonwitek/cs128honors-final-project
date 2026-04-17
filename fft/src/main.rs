aee238e2786e174ba1758c61b115ab069c251bab
jason <jasonwitek2007@gmail.com>
2026-04-16 20:13:02 -0500

    Made twiddle factor helper

--- fft/src/main.rs
+++ fft/src/main.rs
@@ -1,3 +1,7 @@
+use std::{f64::consts::PI, vec};
+
+use num_complex::{Complex, Complex64};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -9,8 +13,22 @@ fn fft() {
 }
 
 // this is the twiddle factor helper
-fn twiddle_factor() {
-
+// Takes in a pair of vectors of the pre seperated odd / even indexes,
+// Returns twiddle factors with first being odds and seond being evens
+fn twiddle_factor(oddsevens: (Vec<f64>, Vec<f64>)) -> (Vec<Complex64>, Vec<Complex64>) {
+    let odds: Vec<f64> = oddsevens.0;
+    let evens: Vec<f64> = oddsevens.1;
+    let mut twiddleodds: Vec<Complex64> = vec![];
+    let mut twiddleevens: Vec<Complex64> = vec![];
+    for k in 0..odds.len() {
+        let realcomp: f64 = (2.0 * PI * k as f64) / odds.len() as f64;
+        twiddleodds.push(Complex { re: realcomp.cos() , im: -realcomp.sin() });
+    }
+    for k in 0..evens.len() {
+        let realcomp: f64 = (2.0 * PI * k as f64) / evens.len() as f64;
+        twiddleevens.push(Complex { re: realcomp.cos() , im: -realcomp.sin() });
+    }
+    return (twiddleodds, twiddleevens);
 }
 
 // this is the bit reversal
