a378cab46e87d56db18c3d1d48464d794087368a
jason <jasonwitek2007@gmail.com>
2026-04-16 20:27:46 -0500

    Add temporary array splitter method

--- fft/src/main.rs
+++ fft/src/main.rs
@@ -8,8 +8,10 @@ fn main() {
 
 // this will be our main function
 // We should have it return the DFT vector so we can print it out in our main function
-fn fft() {
-
+fn fft(input_array: Vec<f64>) -> Vec<Complex64> {
+    let odds_evens = splitarray(input_array);
+    let twiddle_factors: (Vec<Complex64>, Vec<Complex64>) = twiddle_factor((odds_evens.0, odds_evens.1));
+    todo!();
 }
 
 // this is the twiddle factor helper
@@ -18,17 +20,31 @@ fn fft() {
 fn twiddle_factor(oddsevens: (Vec<f64>, Vec<f64>)) -> (Vec<Complex64>, Vec<Complex64>) {
     let odds: Vec<f64> = oddsevens.0;
     let evens: Vec<f64> = oddsevens.1;
-    let mut twiddleodds: Vec<Complex64> = vec![];
-    let mut twiddleevens: Vec<Complex64> = vec![];
+    let mut twiddle_odds: Vec<Complex64> = vec![];
+    let mut twiddle_evens: Vec<Complex64> = vec![];
     for k in 0..odds.len() {
         let realcomp: f64 = (2.0 * PI * k as f64) / odds.len() as f64;
-        twiddleodds.push(Complex { re: realcomp.cos() , im: -realcomp.sin() });
+        twiddle_odds.push(Complex { re: realcomp.cos() , im: -realcomp.sin() });
     }
     for k in 0..evens.len() {
         let realcomp: f64 = (2.0 * PI * k as f64) / evens.len() as f64;
-        twiddleevens.push(Complex { re: realcomp.cos() , im: -realcomp.sin() });
+        twiddle_evens.push(Complex { re: realcomp.cos() , im: -realcomp.sin() });
     }
-    return (twiddleodds, twiddleevens);
+    return (twiddle_odds, twiddle_evens);
+}
+
+// This is a temporary splitter function while we work on the iterative approach
+fn splitarray(input_array: Vec<f64>) -> (Vec<f64>, Vec<f64>) {
+    let mut odds: Vec<f64> = vec![];
+    let mut evens: Vec<f64> = vec![];
+    for i in 0..input_array.len() {
+        if i % 2 == 0 {
+            evens.push(input_array[i]);
+        } else {
+            odds.push(input_array[i]);
+        }
+    }
+    return (odds, evens);
 }
 
 // this is the bit reversal
